6456978c950e73ddf452130844ec363a722fabee
VitorAvelar1 <vitorhugo.diniz@hotmail.com>
2025-05-14 21:16:00 -0300

    Projeto JokenPo finalizado.

---
+++ script.js
@@ -0,0 +1,39 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const opponentScore = document.querySelector('#opponent-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
+const playHuman = (humanChoice) => {
+    playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    console.log('Humano: ' + human + "Maquina: " + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Deu empate."
+    } else if (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK || human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS || human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = "Você venceu!"
+    } else {
+        machineScoreNumber++
+        opponentScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Você perdeu."
+    }
+}
